f744ed7199ab74ad25ea11f67b39fab7aa30db08
Andreson Souza <aoliveiraso@minsait.com>
2025-10-21 12:30:11 -0300

    first commit

---
+++ src/test/java/com/andresonsouza/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresonsouza.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
